cddc47e26268150d32d7981f30e179ae3a049eb1
Oliver Eberle <oeberle@cellmobs.com>
2014-09-17 11:08:31 -0700

    initial commit

---
+++ mongodb-vl/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-vl"
+description 'Simple cookbook for MongoDB Berkshelf based install'
+maintainer  "Vertical Logic"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
